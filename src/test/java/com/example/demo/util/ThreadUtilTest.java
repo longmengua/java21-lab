72b2f862701c602be9cff86552153bca34110f74
Waltor <longmengua@users.noreply.github.com>
2025-07-25 02:38:43 +0800

    feat: update

--- src/test/java/com/example/demo/util/ThreadUtilTest.java
+++ src/test/java/com/example/demo/util/ThreadUtilTest.java
@@ -5,41 +5,37 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.BiConsumer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ThreadUtilTest {
 
-    // @Test
-    // void testRunWithBasicThreads() throws InterruptedException {
-    // AtomicInteger counter = new AtomicInteger(0);
-    // Runnable task = () -> counter.addAndGet(2);
+    @Test
+    void testRunWithBasicThreads() throws InterruptedException {
+        AtomicInteger counter = new AtomicInteger(0);
+        Runnable task = () -> counter.addAndGet(2);
 
-    // ThreadUtil.runWithBasicThreads(task, task, task);
-    // Thread.sleep(500); // Wait for threads to finish
-    // assertEquals(6, counter.get());
-    // }
+        ThreadUtil.runWithBasicThreads(task, task, task);
+        Thread.sleep(500); // Wait for threads to finish
+        assertEquals(6, counter.get());
+    }
 
-    // @Test
-    // void testRunWithFixedThreadPool() {
-    // AtomicInteger counter = new AtomicInteger(0);
-    // Runnable task = () -> {
-    // System.out.println("Task executed by: " + Thread.currentThread().getName());
-    // counter.addAndGet(2);
-    // };
+    @Test
+    void testRunWithFixedThreadPool() {
+        AtomicInteger counter = new AtomicInteger(0);
+        Runnable task = () -> {
+            System.out.println("Task executed by: " + Thread.currentThread().getName());
+            counter.addAndGet(2);
+        };
 
-    // ThreadUtil.runWithFixedThreadPool(task, task, task);
-    // assertEquals(6, counter.get());
-    // }
+        ThreadUtil.runWithFixedThreadPool(task, task, task);
+        assertEquals(6, counter.get());
+    }
 
     @Test
     void testRunWithForkJoinPoolv1() {
@@ -64,65 +60,65 @@ class ThreadUtilTest {
         System.out.println("Test execution time: " + (endTime - startTime) + "milliseconds");
     }
 
-    // @Test
-    // void testRunWithCachedThreads() {
-    // AtomicInteger counter = new AtomicInteger(0);
-    // Runnable task = () -> {
-    // System.out.println("Task executed by: " + Thread.currentThread().getName());
-    // counter.addAndGet(2);
-    // };
+    @Test
+    void testRunWithCachedThreads() {
+        AtomicInteger counter = new AtomicInteger(0);
+        Runnable task = () -> {
+            System.out.println("Task executed by: " + Thread.currentThread().getName());
+            counter.addAndGet(2);
+        };
 
-    // ThreadUtil.runWithCachedThreads(task, task, task);
-    // assertEquals(6, counter.get());
-    // }
+        ThreadUtil.runWithCachedThreads(task, task, task);
+        assertEquals(6, counter.get());
+    }
 
-    // @Test
-    // void testRunWithVirtualThread() {
-    // AtomicInteger counter = new AtomicInteger(0);
-    // Runnable task = counter::incrementAndGet;
+    @Test
+    void testRunWithVirtualThread() {
+        AtomicInteger counter = new AtomicInteger(0);
+        Runnable task = counter::incrementAndGet;
 
-    // ThreadUtil.runWithVirtualThread(task, task, task);
-    // assertEquals(3, counter.get());
-    // }
+        ThreadUtil.runWithVirtualThread(task, task, task);
+        assertEquals(3, counter.get());
+    }
 
-    // @Test
-    // void testRunWithThreadOfVirtual() {
-    // AtomicInteger counter = new AtomicInteger(0);
-    // Runnable task = counter::incrementAndGet;
+    @Test
+    void testRunWithThreadOfVirtual() {
+        AtomicInteger counter = new AtomicInteger(0);
+        Runnable task = counter::incrementAndGet;
 
-    // ThreadUtil.runWithThreadOfVirtual(task, task, task);
-    // assertEquals(3, counter.get());
-    // }
+        ThreadUtil.runWithThreadOfVirtual(task, task, task);
+        assertEquals(3, counter.get());
+    }
 
-    // @Test
-    // void testRunWithSynchronized() {
-    // List<String> log = new ArrayList<>();
-    // AtomicInteger counter = new AtomicInteger(0);
-    // Runnable task = counter::incrementAndGet;
+    @Test
+    void testRunWithSynchronized() {
+        List<String> log = new ArrayList<>();
+        AtomicInteger counter = new AtomicInteger(0);
+        Runnable task = counter::incrementAndGet;
 
-    // ThreadUtil.runWithSynchronized(log, task, task, task);
-    // assertEquals(3, counter.get());
-    // assertFalse(log.isEmpty());
-    // }
+        ThreadUtil.runWithSynchronized(log, task, task, task);
+        assertEquals(3, counter.get());
+        assertFalse(log.isEmpty());
+    }
 
-    // @Test
-    // void testRunWithReentrantLock() {
-    // ReentrantLock lock = new ReentrantLock();
-    // AtomicInteger counter = new AtomicInteger(0);
-    // Runnable task = counter::incrementAndGet;
+    @Test
+    void testRunWithReentrantLock() {
+        ReentrantLock lock = new ReentrantLock();
+        AtomicInteger counter = new AtomicInteger(0);
+        Runnable task = counter::incrementAndGet;
 
-    // ThreadUtil.runWithReentrantLock(lock, task, task, task);
-    // assertEquals(3, counter.get());
-    // }
+        ThreadUtil.runWithReentrantLock(lock, task, task, task);
+        assertEquals(3, counter.get());
+    }
 
-    // @Test
-    // void testRunWithCompletableFuture() {
-    // AtomicInteger counter = new AtomicInteger(0);
-    // Runnable task = counter::incrementAndGet;
+    @Test
+    void testRunWithCompletableFuture() {
+        AtomicInteger counter = new AtomicInteger(0);
+        Runnable task = counter::incrementAndGet;
 
-    // CompletableFuture<Void> future = ThreadUtil.runWithCompletableFuture(task,
-    // task, task);
-    // future.join();
-    // assertEquals(3, counter.get());
-    // }
+        CompletableFuture<Void> future = ThreadUtil.runWithCompletableFuture(task,
+                task, task);
+        future.join();
+        assertEquals(3, counter.get());
+    }
 }
